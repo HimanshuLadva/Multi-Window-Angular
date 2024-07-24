0100c2e642ae88e30f4f53eef8e4686bc94ef9fa
HimanshuLadva <himanshuladva2241@gmail.com>
2024-07-24 21:29:20 +0530

    add taskbar close

--- src/app/taskbar/taskbar.component.ts
+++ src/app/taskbar/taskbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DialogService } from '../dialog.service';
 import { filter } from 'rxjs';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-taskbar',
@@ -26,5 +27,7 @@ export class TaskbarComponent {
       this.dialogService.openDialogInAdd(page, data);
   }
 
-  close() {}
+  close(dialog: MatDialog) {
+     this.dialogService.close(dialog);
+  }
 }
